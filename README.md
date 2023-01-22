01c91b67069f271e882ef0d039f54d61b15a83bd
Thomas <selligtom@gmail.com>
2023-01-22 18:28:19 +0100

    setup the repo

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # react-parcel-starter
 A simple react application built with parcel js 
+Just clone this repo and start building your own React app
+
+To run the application, copy this command to your terminal:
+```
+npm start
+```
---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class App extends Component {
+    render() {
+        return (
+            <div className="app-container">
+                <h1> <a href="https://github.com/selligtom/react-parcel-starter"> React Parcel starter </a> </h1>
+                <p>Made by <a href="https://github.com/selligtom">@selligtom</a> </p>
+            </div>
+        )
+    }
+}
+
+export default App
